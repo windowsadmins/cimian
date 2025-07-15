513bcacb66b8eafa516ece0d14ae189ee1154623
Rod Christiansen <rod@focused.systems>
2025-07-15 00:07:23 -0700

    Remove WPF application files and project configuration for Cimian.Status, including App.xaml, App.xaml.cs, Program.cs, project file, and application icon. This change effectively deletes the entire WPF application structure.

--- cmd/cimistatus/App.xaml.cs
+++ cmd/cimistatus/App.xaml.cs
@@ -1,52 +1,15 @@
-using System;
 using System.Windows;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace CimianStatus
+namespace Cimian.Status
 {
     public partial class App : Application
     {
-        private IHost? _host;
-        private IServiceProvider? _serviceProvider;
-
         protected override void OnStartup(StartupEventArgs e)
         {
-            // Configure dependency injection
-            _host = Host.CreateDefaultBuilder()
-                .ConfigureServices((context, services) =>
-                {
-                    services.AddSingleton<StatusServer>();
-                    services.AddSingleton<StatusViewModel>();
-                    services.AddTransient<MainWindow>();
-                    services.AddLogging(builder =>
-                    {
-                        builder.AddDebug();
-                        builder.AddConsole();
-                        builder.SetMinimumLevel(LogLevel.Information);
-                    });
-                })
-                .Build();
-
-            _serviceProvider = _host.Services;
-
-            // Start the status server
-            var statusServer = _serviceProvider.GetRequiredService<StatusServer>();
-            _ = statusServer.StartAsync(); // Fire and forget - we don't need to await this
-
-            // Create and show main window
-            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
-            MainWindow = mainWindow;
-            mainWindow.Show();
-
+            // Set theme preference based on system settings - use Light as default
+            ModernWpf.ThemeManager.Current.ApplicationTheme = ModernWpf.ApplicationTheme.Light;
+            
             base.OnStartup(e);
-        }
-
-        protected override void OnExit(ExitEventArgs e)
-        {
-            _host?.Dispose();
-            base.OnExit(e);
         }
     }
 }
