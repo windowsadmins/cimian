11fbc2ff41794171918d65e1a2166f028b42b47e
Rod Christiansen <rod@focused.systems>
2025-08-18 10:07:23 -0700

    Fix source tracking to use manifest filename when name field is empty
    - Modify manifest.AuthenticatedGet to set mf.Name to filename if empty
    - Remove 'unknown-manifest' fallbacks in source tracking
    - This fixes the issue where all sources showed 'unknown-manifest'

--- pkg/utils/yaml.go
+++ pkg/utils/yaml.go
@@ -3,6 +3,7 @@
 package utils
 
 import (
+	"fmt"
 	"strings"
 
 	"gopkg.in/yaml.v3"
@@ -13,17 +14,34 @@ type LiteralString string
 
 // MarshalYAML implements the yaml.Marshaler interface.
 func (ls LiteralString) MarshalYAML() (interface{}, error) {
-	// Normalize line endings
-	value := strings.ReplaceAll(string(ls), "\r\n", "\n")
-	// Trim any leading/trailing whitespace
-	value = strings.TrimSpace(value)
+	value := string(ls)
 
-	node := &yaml.Node{
-		Kind:  yaml.ScalarNode,
-		Tag:   "!!str",
-		Value: value,
-		Style: yaml.LiteralStyle,
+	// Create a yaml.Node with literal style for all non-empty strings
+	if value != "" {
+		// Convert escaped sequences to actual characters to make content more literal-friendly
+		value = strings.ReplaceAll(value, "\\n", "\n")
+		value = strings.ReplaceAll(value, "\\r", "\r")
+		value = strings.ReplaceAll(value, "\\t", "\t")
+
+		// Always use LiteralStyle to preserve block formatting
+		node := &yaml.Node{
+			Kind:  yaml.ScalarNode,
+			Tag:   "!!str",
+			Value: value,
+			Style: yaml.LiteralStyle,
+		}
+		return node, nil
 	}
 
-	return node, nil
+	// For empty strings, return as-is
+	return value, nil
+}
+
+// UnmarshalYAML implements the yaml.Unmarshaler interface.
+func (ls *LiteralString) UnmarshalYAML(node *yaml.Node) error {
+	if node.Kind != yaml.ScalarNode {
+		return fmt.Errorf("cannot unmarshal %v into LiteralString", node.Kind)
+	}
+	*ls = LiteralString(node.Value)
+	return nil
 }
